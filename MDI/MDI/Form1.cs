47ef837c0acc0ed8be1298c5e002d914d2a39218
cuthanhcam <cuthanhcam04@gmail.com>
2024-12-06 08:59:51 +0700

    Add Session_05

---
+++ MDI/MDI/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Bitmap file|*.bmp|JPEG file|*.jpg";
+            if (ofd.ShowDialog() == DialogResult.OK )
+            {
+                Form2 frm = new Form2(ofd.FileName);
+                frm.MdiParent = this;
+                frm.Show();
+            }
+        }
+
+        private void cassToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+        private void tileHonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+    }
+}
